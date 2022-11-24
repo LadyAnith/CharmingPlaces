25e81ecd8715b901e461717749d0ced134f7970a
LadyAnith <ladyanith@gmail.com>
2022-11-24 19:30:18 +0100

    Creación de un Menú para hacer Log Out. Añadido el logging a través de una cuenta de Google.

--- app/src/main/java/com/example/charmingplaces/activities/MainActivity.java
+++ app/src/main/java/com/example/charmingplaces/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.charmingplaces.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,19 +14,37 @@ import android.widget.Button;
 import android.widget.PopupWindow;
 
 import com.example.charmingplaces.R;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     Button boton;
     Button boton2;
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boton = (Button) findViewById(R.id.btnInicio);
+        //boton = (Button) findViewById(R.id.btnInicio);
         boton2 = (Button) findViewById(R.id.btnRegistro);
+        /*
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +55,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+*/
+        findViewById(R.id.signInButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signin();
+            }
+        });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
 
         boton2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +79,62 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+            updateUI(currentUser);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e){
+                Log.w(TAG, "Google sign in failed", e);
+            }
+
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "sign in with Credential : Success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else{
+                            Log.d(TAG, "sign in with Credential : Failure", task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void signin(){
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, RC_SIGN_IN);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        FirebaseUser curUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(curUser != null){
+            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/charmingplaces/activities/MenuActivity.java
+++ app/src/main/java/com/example/charmingplaces/activities/MenuActivity.java
@@ -1,13 +1,19 @@
 package com.example.charmingplaces.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.charmingplaces.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
     ConstraintLayout btnCapture;
@@ -48,5 +54,32 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_logout:
+                Log.d("DESLOGUEADO", "APAGADOOOOOOOOOOOOOOOOOOOOOO");
+                signOut();
+                break;
+            case R.id.action_user:
+                Toast.makeText(this, "USUARIO", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
